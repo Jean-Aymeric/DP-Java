401ef6a76f20423bd8275312001896d1ec89aa34
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2023-03-08 14:03:20 +0100

    Interpreter start

--- src/com/jad/Main.java
+++ src/com/jad/Main.java
@@ -7,6 +7,7 @@ import com.jad.product.operation.complex.Stack;
 import com.jad.product.operation.complex.StickLeftToRight;
 import com.jad.product.operation.complex.StickTopToBottom;
 import com.jad.product.operation.simple.CropBottomRightCorner;
+import com.jad.product.operation.simple.CropTopLeftCorner;
 import com.jad.product.operation.simple.Sand;
 
 public class Main {
@@ -17,16 +18,10 @@ public class Main {
         Product emptyCircle = new EmptyCircle('G');
         Product filledCircle = new FilledCircle('Y');
 
-        emptySquare.draw();
-//        filledCircle.draw();
-//        emptyCircle.draw();
-//        filledCircle.draw();
         IProduct p = new Sand().execute(new CropBottomRightCorner().execute(filledCircle));
-        p.draw();
         p = new Stack().execute(filledSquare, p, emptyCircle);
-        p.draw();
         p = new StickTopToBottom().execute(p, filledCircle, emptySquare);
-        p = new StickLeftToRight().execute(p, p, p, p, p);
+        p = new CropTopLeftCorner().execute(new StickLeftToRight().execute(filledCircle, p, p, p, p, filledCircle));
         p.draw();
 
         System.out.println(p.getDescription());
