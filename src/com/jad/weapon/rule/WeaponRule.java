d15ba41839238574bcb0970db0b2b84932932d09
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2023-03-10 19:53:42 +0100

    Composite specification start

---
+++ src/com/jad/weapon/rule/WeaponRule.java
@@ -0,0 +1,6 @@
+package com.jad.weapon.rule;
+
+import com.jad.compositespecification.CompositeSpecification;
+import com.jad.weapon.IWeapon;
+
+public abstract class WeaponRule extends CompositeSpecification<IWeapon> { }
